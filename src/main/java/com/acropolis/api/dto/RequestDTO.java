502f930ee50819e51eab3f21c58faa0456419740
Pankaj Kurmi <pankajkurmi1408@gmail.com>
2026-05-26 15:45:15 +0530

    Fix RequestDTO with proper JSON annotations

--- src/main/java/com/acropolis/api/dto/RequestDTO.java
+++ src/main/java/com/acropolis/api/dto/RequestDTO.java
@@ -1,9 +1,18 @@
 package com.acropolis.api.dto;
 
 import java.util.List;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class RequestDTO {
+    @JsonProperty("data")
     private List<String> data;
+
+    public RequestDTO() {
+    }
+
+    public RequestDTO(List<String> data) {
+        this.data = data;
+    }
 
     public List<String> getData() {
         return data;
